f4d18251b9cacf3adebeadf09f3f087f4c522ca2
Pablo <padanguina@gmail.com>
2022-01-26 18:58:01 -0300

    Servicio y servidor de tareas listo

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { Task } from 'src/app/task';
 import { TASKS } from 'src/app/mock-tasks';
 //Importamos librería observable, para permitirnos conocer cuando ha sido modificado
@@ -10,11 +11,20 @@ import { Observable, of } from 'rxjs';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/tasks' //Dirección del json-server
+
+
+  constructor(
+    private http:HttpClient // Inicializar el método HttpClient
+  ) { }
 
   //Definir nuevos métodos para llamar a las tareas
   getTasks(): Observable <Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+
+/*    const tasks = of(TASKS);
+    return tasks;*/
+
+return this.http.get<Task[]>(this.apiUrl)
+
   }
 }
